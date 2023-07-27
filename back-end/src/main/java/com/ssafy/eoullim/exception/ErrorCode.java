2625947311e56a455fab0ae06a42354282d5d66b
유연석 <ysyoo627@naver.com>
2023-07-28 08:54:59 +0900

    BE-FIX: Like and dev-back Merge #S09P12C207-159

--- back-end/src/main/java/com/ssafy/eoullim/exception/ErrorCode.java
+++ back-end/src/main/java/com/ssafy/eoullim/exception/ErrorCode.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid token"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Invalid password"),
     INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "Invalid Permission"),
@@ -17,6 +16,7 @@ public enum ErrorCode {
     DATA_NOT_FOUND(HttpStatus.NOT_FOUND, "Data not founded"),
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "Duplicated user name"),
     DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Database error occurs"),
+    CONNECTION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "API Connection error occurs"),
     LIKE_ALREADY_FOUND(HttpStatus.FOUND, "Like Relationship already founded"),
     ;
 
