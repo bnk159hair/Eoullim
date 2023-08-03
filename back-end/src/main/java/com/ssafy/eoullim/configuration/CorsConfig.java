e3497a74b0823613fcd90a45a13d1c38d8bfb13f
고우민 <kwm907@gmail.com>
2023-08-03 14:22:56 +0900

    BE-FIX: configuration.setAllowedOrigins(List.of("*"));

--- back-end/src/main/java/com/ssafy/eoullim/configuration/CorsConfig.java
+++ back-end/src/main/java/com/ssafy/eoullim/configuration/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Content-Type", "Authorization"));
         configuration.addAllowedHeader("Access-Control-Allow-Credentials");
