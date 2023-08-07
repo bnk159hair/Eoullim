7859bc12bccabe34c127c0270b5e403f6acece10
김성준 <gkxm0443@naver.com>
2023-08-07 12:10:54 +0900

    FE-FIX : 친구목록 로직 수정 S09P12C207-464

---
+++ front-end/src/components/record/RecordListItemStyles.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import RecordBox from '../../assets/box/woodbox.jpg'
+
+export const RecordItemContainer= styled.div`
+    height : 250px;
+    width: 1100px;
+    background-image: url(${RecordBox});
+    background-size: cover;
+    border-radius: 25px;
+`
