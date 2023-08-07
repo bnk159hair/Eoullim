633b73b1a2a0a49de47c379e05019644435f6b04
kingturtle0 <dhdnqkrtk22@naver.com>
2023-08-07 23:35:11 +0900

    FE-FEAT : 회원가입 모달로 변경

--- front-end/src/components/login/Login.tsx
+++ front-end/src/components/login/Login.tsx
@@ -1,22 +1,24 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
-import { tokenState } from '../../atoms/Auth';
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { tokenState } from "../../atoms/Auth";
 import {
   FormContainer,
   LoginButton,
   SignupContainer,
   SignupAnchor,
-} from './LoginStyles';
-import { TextField, InputAdornment } from '@mui/material';
-import { AccountCircle } from '@mui/icons-material';
-import KeyIcon from '@mui/icons-material/Key';
-import { BASEURL } from '../../apis/urls';
+} from "./LoginStyles";
+import { TextField, InputAdornment } from "@mui/material";
+import { AccountCircle } from "@mui/icons-material";
+import KeyIcon from "@mui/icons-material/Key";
+import { BASEURL } from "../../apis/urls";
+import SignUpModal from "./SignupModal";
 
 const Login = () => {
-  const [userName, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [userName, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isModalOpen, setModalOpen] = useState(false);
   const navigate = useNavigate();
   const [token, setToken] = useRecoilState(tokenState);
 
@@ -26,70 +28,75 @@ const Login = () => {
       .post(`${BASEURL}/users/login`, { userName, password })
       .then((response) => {
         setToken(response.data.result);
-        navigate('/profile');
+        navigate("/profile");
       })
       .catch((error) => {
-        alert('아이디와 비밀번호를 확인해주세요.');
+        alert("아이디와 비밀번호를 확인해주세요.");
       });
   };
 
-  const handleSignup = () => {
-    navigate('/signup');
+  const handleModalOpen = () => {
+    setModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(false);
   };
 
   return (
     <>
       <FormContainer onSubmit={handleLogin}>
         <TextField
-          label="아이디"
-          variant="filled"
-          color="success"
-          margin="dense"
+          label='아이디'
+          variant='filled'
+          color='success'
+          margin='dense'
           value={userName}
           sx={{
-            bgcolor: 'white',
-            borderRadius: '5px',
-            width: '80%',
+            bgcolor: "white",
+            borderRadius: "5px",
+            width: "80%",
           }}
           onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
             setUsername(event.target.value)
           }
           InputProps={{
             startAdornment: (
-              <InputAdornment position="start">
+              <InputAdornment position='start'>
                 <AccountCircle />
               </InputAdornment>
             ),
           }}
         />
         <TextField
-          label="비밀번호"
-          variant="filled"
-          color="success"
-          margin="dense"
-          type="password"
+          label='비밀번호'
+          variant='filled'
+          color='success'
+          margin='dense'
+          type='password'
           value={password}
           sx={{
-            bgcolor: 'white',
-            borderRadius: '5px',
-            width: '80%',
+            bgcolor: "white",
+            borderRadius: "5px",
+            width: "80%",
           }}
           onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
             setPassword(event.target.value)
           }
           InputProps={{
             startAdornment: (
-              <InputAdornment position="start">
+              <InputAdornment position='start'>
                 <KeyIcon />
               </InputAdornment>
             ),
           }}
         />
-        <LoginButton type="submit">로그인</LoginButton>
+        <LoginButton type='submit'>로그인</LoginButton>
       </FormContainer>
       <SignupContainer>
         <span>아직 회원이 아니신가요?</span>
-        <SignupAnchor onClick={handleSignup}>회원가입</SignupAnchor>
+        <SignupAnchor onClick={handleModalOpen}>회원가입</SignupAnchor>
+        {isModalOpen && <SignUpModal onClose={handleModalClose} />}
       </SignupContainer>
     </>
   );
