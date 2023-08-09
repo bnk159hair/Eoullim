670386a9757fd5c4d5f2901b34556f23e187f37e
김하영 <bnk159hair@naver.com>
2023-08-09 13:35:20 +0900

    BE-FEAT : openvidu와 API 서버 통합 오류 수정

--- back-end/src/main/java/com/ssafy/eoullim/dto/request/MatchRequest.java
+++ back-end/src/main/java/com/ssafy/eoullim/dto/request/MatchRequest.java
@@ -2,9 +2,11 @@ package com.ssafy.eoullim.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class MatchRequest {
     private Integer childId;
     private String name;
