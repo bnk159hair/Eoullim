ad18b07aab9fc9752355a6de68c2ba63e643261f
고우민 <kwm907@gmail.com>
2023-08-14 17:53:54 +0900

    BE-FIX: setGuideSeq

--- back-end/src/main/java/com/ssafy/eoullim/repository/RecordRepository.java
+++ back-end/src/main/java/com/ssafy/eoullim/repository/RecordRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.*;
 
 public interface RecordRepository extends JpaRepository<RecordEntity, Integer> {
-    @Query(value = "select r.record_id, r.create_time, r.video_path, c.name, c.school, a.name as animonName " +
+    @Query(value = "select r.record_id, r.create_time, r.video_path, c.name, c.school, a.name as animonName, r.guide_seq, r.timeline " +
             "from record_info as r join child as c on r.participant_id=c.child_id " +
             "join animon as a on c.animon_id=a.animon_id " +
             "where r.master_id = :myId", nativeQuery = true)
