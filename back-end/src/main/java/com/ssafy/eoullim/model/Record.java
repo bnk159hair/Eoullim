101b9c292f609f61180fa94f7052b1c19048f958
김하영 <bnk159hair@naver.com>
2023-08-26 21:00:21 +0900

    BE-modify : record JPA 수정

--- back-end/src/main/java/com/ssafy/eoullim/model/Record.java
+++ back-end/src/main/java/com/ssafy/eoullim/model/Record.java
@@ -1,21 +1,33 @@
 package com.ssafy.eoullim.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
 
-@Setter
-@Getter
-@NoArgsConstructor
 public class Record {
-    private Integer record_id;
-    private LocalDateTime create_time;
+    private String record_id;
+    private String create_time;
     private String video_path;
     private String name;
     private String school;
     private String animonName;
 
+    public Record(String record_id, String create_time, String video_path, String name, String school, String animonName) {
+        this.record_id = record_id;
+        this.create_time = create_time;
+        this.video_path = video_path;
+        this.name = name;
+        this.school = school;
+        this.animonName = animonName;
+    }
+
+    @Override
+    public String toString() {
+        return "Record{" +
+                "record_id='" + record_id + '\'' +
+                ", create_time='" + create_time + '\'' +
+                ", video_path='" + video_path + '\'' +
+                ", name='" + name + '\'' +
+                ", school='" + school + '\'' +
+                ", animonName='" + animonName + '\'' +
+                '}';
+    }
 }
