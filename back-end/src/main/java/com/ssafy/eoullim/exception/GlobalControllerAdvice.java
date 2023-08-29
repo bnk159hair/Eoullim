1a5e92faa9484ca1eac70b4fec823f731fd82f71
김하영 <bnk159hair@naver.com>
2023-08-29 21:16:38 +0900

    BE-modify : randomMatch 리팩토링

--- back-end/src/main/java/com/ssafy/eoullim/exception/GlobalControllerAdvice.java
+++ back-end/src/main/java/com/ssafy/eoullim/exception/GlobalControllerAdvice.java
@@ -1,12 +1,15 @@
 package com.ssafy.eoullim.exception;
 
 import com.ssafy.eoullim.dto.response.Response;
+import io.openvidu.java.client.OpenViduException;
+import io.openvidu.java.client.OpenViduHttpException;
+import io.openvidu.java.client.OpenViduJavaClientException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static com.ssafy.eoullim.exception.ErrorCode.DATABASE_ERROR;
+import static com.ssafy.eoullim.exception.ErrorCode.*;
 
 
 @Slf4j
@@ -27,4 +30,24 @@ public class GlobalControllerAdvice {
         return ResponseEntity.status(DATABASE_ERROR.getStatus())
                 .body(Response.error(DATABASE_ERROR.name()));
     }
+
+    @ExceptionHandler(OpenViduHttpException.class)
+    public ResponseEntity<?> openviduErrorHandler(OpenViduHttpException e) {
+        log.error("Error occurs {}", e.toString());
+        return ResponseEntity.status(OPENVIDU_HTTP_ERROR.getStatus())
+                .body(Response.error(OPENVIDU_HTTP_ERROR.name()));
+    }
+    @ExceptionHandler(OpenViduJavaClientException.class)
+    public ResponseEntity<?> openviduErrorHandler(OpenViduJavaClientException e) {
+        log.error("Error occurs {}", e.toString());
+        return ResponseEntity.status(OPENVIDU_SERVER_ERROR.getStatus())
+                .body(Response.error(OPENVIDU_SERVER_ERROR.name()));
+    }
+    @ExceptionHandler(InterruptedException.class)
+    public ResponseEntity<?> InterruptHandler(InterruptedException e) {
+        log.error("Error occurs {}", e.toString());
+        return ResponseEntity.status(OPENVIDU_SERVER_ERROR.getStatus())
+                .body(Response.error(OPENVIDU_SERVER_ERROR.name()));
+    }
+
 }
